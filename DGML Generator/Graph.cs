20faa00d437d34e42f51249cfe5ee0fb14c3416e
Michael Trittin <michaeltrittin@gmail.com>
2016-06-13 10:02:56 -0400

    DGML Generator

---
+++ DGML Generator/Graph.cs
@@ -0,0 +1,13 @@
+﻿using static DGML.DgmlWriter;
+
+namespace DGML_Generator
+{
+    public struct Graph
+    {
+        public Node[] Nodes;
+        public Link[] Links;
+        public Category[] Categories;
+        public Property[] Properties;
+        public Style[] Styles;
+    }
+}
---
+++ DGML Generator/Link.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Serialization;
+
+namespace DGML_Generator
+{
+    public enum LinkStyle
+    {
+        Solid,
+        Dashed,
+        Hidden
+    }
+    public struct Link
+    {
+        [XmlAttribute]
+        public string Source;
+        [XmlAttribute]
+        public string Target;
+        [XmlAttribute]
+        public string Label;
+        [XmlAttribute]
+        public string Category;
+        [XmlAttribute]
+        public string FetchingParent;
+        public Link(string source, string target, string category = null, string label = null)
+        {
+            Source = source;
+            Target = target;
+            Label = label;
+            Category = category;
+            if (category != null)
+            {
+                FetchingParent = source;
+            }
+            else
+            {
+                FetchingParent = null;
+            }
+        }
+    }
+}
